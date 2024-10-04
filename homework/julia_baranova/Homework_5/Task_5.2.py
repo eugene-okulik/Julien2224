8e0498d6d9be195591353fd0710bfb0f46823e8f
Julien2224 <julia.baranova22242000@gmail.com>
2024-10-04 16:33:07 +0200

    Homework 5 Done

---
+++ homework/julia_baranova/Homework_5/Task_5.2.py
@@ -0,0 +1,13 @@
+text1 = 'результат операции: 42'
+text2 = 'результат операции: 514'
+text3 = 'результат работы программы: 9'
+
+number1_index = text1.index('42')
+number2_index = text2.index('514')
+number3_index = text3.index('9')
+print(int(text1[number1_index:]) + 10)
+print(int(text2[number2_index:]) + 10)
+print(int(text3[number3_index:]) + 10)
+
+
+
---
+++ homework/julia_baranova/Homework_5/Task_5.3.py
@@ -0,0 +1,8 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+st1, st2, st3 = students
+sb1, sb2, sb3 = subjects
+
+text = f'Students {st1}, {st2}, {st3} study these subjects: {sb1}, {sb2}, {sb3}'
+print(text)
