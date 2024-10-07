9e450f402be45982386033f687e467e69d5eda58
Julien2224 <julia.baranova22242000@gmail.com>
2024-10-07 15:39:43 +0300

    Homework 5 Fixes

--- homework/julia_baranova/Homework_5/Task_5.2.py
+++ homework/julia_baranova/Homework_5/Task_5.2.py
@@ -2,9 +2,10 @@ text1 = 'результат операции: 42'
 text2 = 'результат операции: 514'
 text3 = 'результат работы программы: 9'
 
-number1_index = text1.index('42')
-number2_index = text2.index('514')
-number3_index = text3.index('9')
-print(int(text1[number1_index:]) + 10)
-print(int(text2[number2_index:]) + 10)
-print(int(text3[number3_index:]) + 10)
+number1 = int(text1[text1.index(':')+1:].strip())
+number2 = int(text2[text2.index(':')+1:].strip())
+number3 = int(text3[text3.index(':')+1:].strip())
+
+print(number1 + 10)
+print(number2 + 10)
+print(number3 + 10)
