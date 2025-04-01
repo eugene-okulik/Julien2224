acf602a42570877dcaf519ffcbbf7334e889e2e6
Julien2224 <julia.baranova22242000@gmail.com>
2025-04-01 14:19:21 +0200

    Homework 11 Done

---
+++ homework/julia_baranova/Homework_11/Task_11.1.py
@@ -0,0 +1,26 @@
+class Book():
+    material = 'бумага'
+    is_text = True
+
+    def __init__ (self, title, author, pages, ISBN, reserved):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.ISBN = ISBN
+        self.reserved = reserved
+
+    def print_details(self):
+        return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages},'
+      f' материал: {self.material}{', зарезервирована' if self.reserved else ''}')
+
+book_1 = Book('Идиот', 'Достоевский', '500', '978-0-6-269366-2', True)
+book_2 = Book('1984', 'Оруэл', '328','978-0-452-28423-4',False)
+book_3 = Book('Мастер и Маргарита','Булгаков','384','978-0-316-76948-0',False)
+book_4 = Book('Солярис','Лем','224', '978-0-15-602760-1',False)
+book_5 = Book('Над пропастью во ржи','Сэлинджер','277', '978-5-389-7423-4',False)
+
+print(book_1.print_details())
+print(book_2.print_details())
+print(book_3.print_details())
+print(book_4.print_details())
+print(book_5.print_details())
---
+++ homework/julia_baranova/Homework_11/Task_11.2.py
@@ -0,0 +1,45 @@
+from abc import abstractmethod,ABC
+
+class Book(ABC):
+    material = 'бумага'
+    is_text = True
+
+    def __init__(self, title, author, pages,isbn, reserved):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.isbn = isbn
+        self.reserved = reserved
+
+    @abstractmethod
+    def print_details(self):
+        pass
+
+class SchoolTextbook(Book):
+
+    def __init__(self, title, author, pages, isbn, reserved, subject, class_number, exercises):
+        super().__init__(title, author, pages, isbn, reserved)
+        self.subject = subject
+        self.class_number = class_number
+        self.exercises = exercises
+
+
+    def print_details(self):
+         return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages},'
+                 f' предмет: {self.subject}, класс: {self.class_number}{', зарезервирована' if self.reserved else ''}')
+
+
+school_text_book_1 = SchoolTextbook('Алгебра', 'Иванов', '200', '978-0-6-269366-2',
+                                    True, 'Математика', '9',True)
+school_text_book_2 = SchoolTextbook('Геометрия', 'Петров', '328','978-0-452-28423-4',
+                                    False,'Математика', '10', True)
+school_text_book_3 = SchoolTextbook('Русский язык','Сидоров','384','978-0-316-76948-0',
+                                    False, 'Русский язык', '8', False)
+school_text_book_4 = SchoolTextbook('Бег','Козлов','224', '978-0-15-602760-1',
+                                    False, 'Физкультура','7', False)
+
+
+print(school_text_book_1.print_details())
+print(school_text_book_2.print_details())
+print(school_text_book_3.print_details())
+print(school_text_book_4.print_details())
