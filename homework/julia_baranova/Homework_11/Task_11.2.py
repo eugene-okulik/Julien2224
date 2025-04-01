44f6fd724d974915443511cbf58ac4f7b5225e6b
Julien2224 <julia.baranova22242000@gmail.com>
2025-04-01 14:34:27 +0200

    Homework 11 Done

--- homework/julia_baranova/Homework_11/Task_11.2.py
+++ homework/julia_baranova/Homework_11/Task_11.2.py
@@ -1,4 +1,5 @@
-from abc import abstractmethod,ABC
+from abc import abstractmethod, ABC
+
 
 class Book(ABC):
     material = 'бумага'
@@ -15,6 +16,7 @@ class Book(ABC):
     def print_details(self):
         pass
 
+
 class SchoolTextbook(Book):
 
     def __init__(self, title, author, pages, ISBN, reserved, subject, class_number, exercises):
@@ -23,7 +25,6 @@ class SchoolTextbook(Book):
         self.class_number = class_number
         self.exercises = exercises
 
-
     def print_details(self):
          return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages}, '
                  f'предмет: {self.subject}, класс: {self.class_number}'
@@ -31,13 +32,13 @@ class SchoolTextbook(Book):
 
 
 school_text_book_1 = SchoolTextbook('Алгебра', 'Иванов', '200', '978-0-6-269366-2',
-                                    True, 'Математика', '9',True)
-school_text_book_2 = SchoolTextbook('Геометрия', 'Петров', '328','978-0-452-28423-4',
-                                    False,'Математика', '10', True)
-school_text_book_3 = SchoolTextbook('Русский язык','Сидоров','384','978-0-316-76948-0',
+                                    True, 'Математика', '9', True)
+school_text_book_2 = SchoolTextbook('Геометрия', 'Петров', '328', '978-0-452-28423-4',
+                                    False, 'Математика', '10', True)
+school_text_book_3 = SchoolTextbook('Русский язык', 'Сидоров', '384', '978-0-316-76948-0',
                                     False, 'Русский язык', '8', False)
-school_text_book_4 = SchoolTextbook('Бег','Козлов','224', '978-0-15-602760-1',
-                                    False, 'Физкультура','7', False)
+school_text_book_4 = SchoolTextbook('Бег', 'Козлов', '224', '978-0-15-602760-1',
+                                    False, 'Физкультура', '7', False)
 
 
 print(school_text_book_1.print_details())
