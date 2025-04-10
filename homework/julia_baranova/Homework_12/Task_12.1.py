ae49cffb6e91b5a255e96748f3de91806cf9477c
Julien2224 <julia.baranova22242000@gmail.com>
2025-04-10 17:48:16 +0200

    Homework 12 Done

--- homework/julia_baranova/Homework_12/Task_12.1.py
+++ homework/julia_baranova/Homework_12/Task_12.1.py
@@ -112,4 +112,4 @@ most_expensive_flower = sorted_by_price[0]
 print(f'Самый дорогой цветок: {most_expensive_flower}')
 red_flowers_from_bouquet = bouquet1.find_red_flowers()
 print("Красные цветы в букете:", ",".join(str(f) for f in red_flowers_from_bouquet)
-     if red_flowers_from_bouquet else "красных цветов в букете нет")
+    if red_flowers_from_bouquet else "красных цветов в букете нет")
