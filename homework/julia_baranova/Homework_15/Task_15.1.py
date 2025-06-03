50abf46fc0ffc80a3d603458ca32c2632bc61412
Julien2224 <julia.baranova22242000@gmail.com>
2025-06-03 19:48:23 +0200

    Homework 15 Done

--- homework/julia_baranova/Homework_15/Task_15.1.py
+++ homework/julia_baranova/Homework_15/Task_15.1.py
@@ -1,48 +1,48 @@
 import mysql.connector as mysql
 
 db = mysql.connect(
-   user='st-onl',
-   passwd='AVNS_tegPDkI5BlB2lW5eASC',
-   host='db-mysql-fra1-09136-do-user-7651996-0.b.db.ondigitalocean.com',
-   port=25060,
-   database='st-onl'
+    user='st-onl',
+    passwd='AVNS_tegPDkI5BlB2lW5eASC',
+    host='db-mysql-fra1-09136-do-user-7651996-0.b.db.ondigitalocean.com',
+    port=25060,
+    database='st-onl'
 )
 
 cursor = db.cursor()
-cursor.execute ("INSERT into students (name, second_name) VALUES ('Julia15', 'Baranova15')")
+cursor.execute("INSERT into students (name, second_name) VALUES ('Julia15', 'Baranova15')")
 student_id = cursor.lastrowid
 cursor.execute("INSERT into books (title) VALUES ('Julia_book1.15')")
 cursor.execute("INSERT into books (title) VALUES ('Julia_book2.15')")
 db.commit()
-cursor.execute ("UPDATE books SET taken_by_student_id = %s  WHERE title = 'Julia_book1.15'",(student_id, ))
-cursor.execute ("UPDATE books SET taken_by_student_id = %s WHERE title = 'Julia_book2.15'",(student_id, ))
-cursor.execute ("INSERT into `groups` (title, start_date, end_date) VALUES ('Julia_group15', 'oct24', 'oct_25')")
+cursor.execute("UPDATE books SET taken_by_student_id = %s  WHERE title = 'Julia_book1.15'", (student_id,))
+cursor.execute("UPDATE books SET taken_by_student_id = %s WHERE title = 'Julia_book2.15'", (student_id,))
+cursor.execute("INSERT into `groups` (title, start_date, end_date) VALUES ('Julia_group15', 'oct24', 'oct_25')")
 group_id = cursor.lastrowid
 db.commit()
-cursor.execute ("UPDATE students SET group_id = %s WHERE id = %s",(group_id,student_id))
+cursor.execute("UPDATE students SET group_id = %s WHERE id = %s", (group_id, student_id))
 
 cursor.execute("INSERT into subjets (title) VALUES ('Julia_sub15.1')")
 subject1_id = cursor.lastrowid
 cursor.execute("INSERT into subjets (title) VALUES ('Julia_sub15.2')")
 subject2_id = cursor.lastrowid
 db.commit()
-cursor.execute ("INSERT into lessons (title, subject_id) VALUES ('lesson15.1', %s )",(subject1_id, ))
+cursor.execute("INSERT into lessons (title, subject_id) VALUES ('lesson15.1', %s )", (subject1_id,))
 lesson1_id = cursor.lastrowid
-cursor.execute ("INSERT into lessons (title, subject_id) VALUES ('lesson15.2', %s )",(subject1_id, ))
+cursor.execute("INSERT into lessons (title, subject_id) VALUES ('lesson15.2', %s )", (subject1_id,))
 lesson2_id = cursor.lastrowid
-cursor.execute ("INSERT into lessons (title, subject_id) VALUES ('lesson15.3', %s )",(subject2_id, ))
+cursor.execute("INSERT into lessons (title, subject_id) VALUES ('lesson15.3', %s )", (subject2_id,))
 lesson3_id = cursor.lastrowid
-cursor.execute ("INSERT into lessons (title, subject_id) VALUES ('lesson15.4', %s )",(subject2_id, ))
+cursor.execute("INSERT into lessons (title, subject_id) VALUES ('lesson15.4', %s )", (subject2_id,))
 lesson4_id = cursor.lastrowid
 db.commit()
-cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson1_id,student_id))
-cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson2_id,student_id))
-cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson3_id,student_id))
-cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson4_id,student_id))
+cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson1_id, student_id))
+cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson2_id, student_id))
+cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson3_id, student_id))
+cursor.execute("INSERT into marks (value, lesson_id, student_id) VALUES (10, %s,%s)", (lesson4_id, student_id))
 db.commit()
-cursor.execute(" SELECT * FROM marks WHERE student_id = %s ", (student_id, ))
+cursor.execute(" SELECT * FROM marks WHERE student_id = %s ", (student_id,))
 print(cursor.fetchall())
-cursor.execute(" SELECT * FROM books WHERE taken_by_student_id = %s", (student_id, ))
+cursor.execute(" SELECT * FROM books WHERE taken_by_student_id = %s", (student_id,))
 print(cursor.fetchall())
 select_query = '''
 SELECT students.id, students.name, students.second_name, books.title, groups.title, subjets.title, lessons.title, marks.value
@@ -56,6 +56,6 @@ WHERE students.id = %s
 GROUP BY
 subjets.id, books.id, marks.id
 '''
-cursor.execute(select_query,(student_id, ))
+cursor.execute(select_query, (student_id,))
 print(cursor.fetchall())
 db.close()
